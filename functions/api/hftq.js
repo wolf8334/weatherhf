1fe65d9d9938aa342f4f58cc960daab7a1c749f2
18013303752 <7957776+ddplus@user.noreply.gitee.com>
2025-09-10 16:33:16 +0800

    初始化代码 访问和风天气 获取数据 日志输出调试

---
+++ functions/api/hftq.js
@@ -0,0 +1,37 @@
+export default {
+    // fetch 函数接收 request 和 env 参数
+    async fetch(request, env) {
+
+        const API_HOST = env.HFTQ_API_HOST;
+        const API_KEY = env.HFTQ_API_KEY;
+        const CITY_ID = '101190101'; // 示例城市ID
+        const API_URL = `${API_HOST}/v7/weather/now?key=${API_KEY}&location=${CITY_ID}`;
+
+        try {
+            // 1. 发起网络请求
+            const response = await fetch(API_URL);
+
+            // 2. 检查响应是否成功
+            if (!response.ok) {
+                console.log(`Failed to fetch data from ${TARGET_URL}. Status: ${response.status}`);
+            }
+
+            // 3. 解析 JSON 数据
+            const data = await response.json();
+
+            // 4. 将解析后的数据转换回 JSON 字符串并返回
+
+            console.log(data);
+            // return new Response(JSON.stringify(data), {
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            // });
+
+        } catch (error) {
+            // 5. 捕获并处理任何错误
+            console.error(error);
+            return new Response('An error occurred while fetching and parsing data.', { status: 500 });
+        }
+    },
+};
